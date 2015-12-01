76fe5229f7a11f2963c69a905f2f5164edd8f4ca
ikros11 <thx!>
2015-12-02 02:15:29 +0900

    First unit test, url mapping, temporary view

--- functional_tests.py
+++ functional_tests.py
@@ -1,25 +1,30 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
-
 	def setUp(self):
-		self.browser = webdriver.Firefox()
+		self.browser = webdriver.FireFox()
 		self.browser.implicitly_wait(3)
-
+		
 	def tearDown(self):
 		self.browser.quit()
-
+	
 	def test_can_start_a_list_and_retrieve_it_later(self):
-# Edith has heard about a cool new online to-do app. She goes
-# to check out its homepage
 		self.browser.get('http://localhost:8000')
-
-# She notices the page title and header mention to-do lists
 		self.assertIn('To-Do', self.browser.title)
-		self.fail('Finish the test!')
-
-# She is invited to enter a to-do item straight away
-
-if __name__ == '__main__':
-	unittest.main(warnings='ignore')
+		
+		header_text = self.browser.find_element_by_tag_name('h1').text
+		self.assertIn('To-Do', header_text)
+		
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		self.assertIn(inputbox.get_attribute('placeholder'), 'Working Item Input')
+		
+		inputbox.send_keys('Buy bird')
+		inputbox.send_keys(Keys.ENTER)
+		
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_element_by_tag_name('tr')
+		
+		self.assertTrue(any(row.text == '1: Buy bird' for row in rows),)
+		self.fail('Finish the Test')

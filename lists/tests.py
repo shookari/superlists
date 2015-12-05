ce7703ce7714b72b7933c6cb85d5577e8c658ce7
ikros11 <thx!>
2015-12-05 12:43:30 +0900

    최소 HTML 을 반환하는 기본 뷰 테스트

--- lists/tests.py
+++ lists/tests.py
@@ -1,9 +1,23 @@
 from django.test import TestCase
 from django.core.urlresolvers import resolve
+from django.http import HttpRequest
+
 from lists.views import home_page
 
 # Create your tests here.
 class HomePageTest(TestCase):
+	# urls 에 설정된 view 함수에 대한 매핑이 예측한대로 되었는지 테스트
 	def test_root_url_resolves_to_home_page_view(self):
 		found = resolve('/')
 		self.assertEqual(found.func, home_page)
+
+	# request 요청 시 응답이 만들어지는 home_page 에서 원하는 결과대로 리턴하는 지 테스트
+	def test_home_page_returns_correct_html(self):
+		request = HttpRequest()
+		response = home_page(request)
+		# response.content 타입은 byte 형식으로 파이선 문자열로 변경하기 위해 b 를 붙여준다.
+		self.assertTrue(response.content.startswith(b'<html>'))
+		self.assertIn(b'<title>To-Do lists</title>', response.content)
+		self.assertTrue(response.content.endswith(b'</html>'))
+
+
--- lists/views.py
+++ lists/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
-def home_page():
-	pass
+
+# TODO : view 함수에서 response 객체를 리턴하도록 수정해야함
+def home_page(request):
+	return HttpResponse("<html><title>To-Do lists</title></html>")
+
